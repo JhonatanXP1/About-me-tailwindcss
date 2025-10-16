99ba011b27f42293f8ef5351293c4eb59cd71899
JhonatanXP1 <jhonatanxpx1@gmail.com>
2025-10-15 22:52:53 -0600

    Update- Configurar entorno

---
+++ pruebas/tailwind.config.js
@@ -0,0 +1,12 @@
+// filepath: c:\Users\jhona\OneDrive\Documentos\GitHub\tailwindcss\pruebas\tailwind.config.js
+module.exports = {
+    content: ["./public/**/*.html"], // Ruta a tus archivos HTML
+    theme: {
+      extend: {
+        colors: {
+          'fondo-oscuro': '#1d1d1d', // Color personalizado
+        },
+      },
+    },
+    plugins: [],
+  };
