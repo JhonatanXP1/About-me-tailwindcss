6ff324542efe790ca5b9144a082d47a1a64780fb
JhonatanEducapp <jdiaz@sistemaeducapp.com>
2025-10-27 17:41:38 -0600

    UPDATE - Animacion. Problematica. 	La animacion del codigo se mostraba 	de manera incorrecta debido a que al terminar

--- pruebas/public/animation.js
+++ pruebas/public/animation.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', function() {
-    let text = `if(action=="I|imagine|it"){ \n iProgramIt(); \n}`;
+    let text = `if(action|==|"I|imagine|it"){ \n iProgramIt(); \n}`;
     let textAux=''; 
     let colorMetria= {
         'if':'text-red-500',
         'action':'',
-        '==':'text-red-500',
+        '|==|':'text-red-500',
         ';':'',
         '(':'text-cyan-500',
         ')':'text-cyan-500',
@@ -20,10 +20,9 @@ window.addEventListener('load', function() {
     const targetFinal = document.getElementById('final');
 
     for (let letra of text) {
+        let letraSec=letra;
         delay += 200; // Incrementa ANTES de cada setTimeout
         setTimeout(() => {
-            textAux+=[' ','\s', '\n', '\t'].includes(letra)? '':letra;
-            
             if (letra === '\n') {
                 if(!breakLine){
                     target.innerHTML +='<br>&nbsp;&nbsp;&nbsp;&nbsp;'; 
@@ -31,14 +30,26 @@ window.addEventListener('load', function() {
                  }else{
                      target.innerHTML +='<br>';
                  } // Agrega salto de línea HTML
+
             } else {
+                letra=(letra=='|')?' ':letra;
                 target.innerHTML += letra;
             }
 
+            textAux+=[' ','\s', '\n', '\t'].includes(letraSec)? '':letraSec;
+
             if  (textAux in colorMetria){
-                console.log(`Coincidencia de color para: "${textAux}"`);
                 target.innerHTML ='';
-                targetFinal.innerHTML += `<span class="${colorMetria[textAux]}">${textAux}</span>`;
+                if(textAux=='iProgramIt()'){
+                    targetFinal.innerHTML += `<span class="${colorMetria[textAux]}"><br>&nbsp;&nbsp;&nbsp;&nbsp;${textAux}</span>`;
+                }else if(textAux==';'){
+                    targetFinal.innerHTML += `<span class="${colorMetria[textAux]}">${textAux}</span><br>`;
+                }else if(textAux.includes('|')){
+                    targetFinal.innerHTML += `<span class="${colorMetria[textAux]}">${textAux.replaceAll('|',' ')}</span>`;
+                }else{
+                    targetFinal.innerHTML += `<span class="${colorMetria[textAux]}">${textAux}</span>`;
+                }
+
                 textAux='';
             }
         }, delay);
