ec06fe34a86247d6da6dcdc5d52aa645a47f1d7d
JhonatanEducapp <jdiaz@sistemaeducapp.com>
2025-10-29 14:09:11 -0600

    Update- Animación de Cambio. Resolucion: 	Se ha avanzado en la animacion para el cambio del primer pint al Portafolio.

--- pruebas/public/animation.js
+++ pruebas/public/animation.js
@@ -1,57 +1,67 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     let text = `if(action|==|"I|imagine|it"){ \n iProgramIt(); \n}`;
-    let textAux=''; 
-    let colorMetria= {
-        'if':'text-red-500',
-        'action':'',
-        '|==|':'text-red-500',
-        ';':'',
-        '(':'text-cyan-500',
-        ')':'text-cyan-500',
-        '"I|imagine|it"':'text-amber-400',
-        '{':'text-fuchsia-400',
-        '}':'text-fuchsia-400',
-        'iProgramIt()':'text-lime-400',
+    let textAux = '';
+    let colorMetria = {
+        'if': 'text-red-500',
+        'action': '',
+        '|==|': 'text-red-500',
+        ';': '',
+        '(': 'text-cyan-500',
+        ')': 'text-cyan-500',
+        '"I|imagine|it"': 'text-amber-400',
+        '{': 'text-fuchsia-400',
+        '}': 'text-fuchsia-400',
+        'iProgramIt()': 'text-lime-400',
     };
-        
+
     let delay = 0; // Comienza en 0
     let breakLine = false;
     const target = document.getElementById('textIntroducction');
     const targetFinal = document.getElementById('final');
+    const animacion_change_top = document.getElementById('animacion-change-top');
+    const animacion_change_bottom = document.getElementById('animacion-change-bottom');
 
     for (let letra of text) {
-        let letraSec=letra;
+        let letraSec = letra;
         delay += 200; // Incrementa ANTES de cada setTimeout
         setTimeout(() => {
             if (letra === '\n') {
-                if(!breakLine){
-                    target.innerHTML +='<br>&nbsp;&nbsp;&nbsp;&nbsp;'; 
+                if (!breakLine) {
+                    target.innerHTML += '<br>&nbsp;&nbsp;&nbsp;&nbsp;';
                     breakLine = true;
-                 }else{
-                     target.innerHTML +='<br>';
-                 } // Agrega salto de línea HTML
+                } else {
+                    target.innerHTML += '<br>';
+                } // Agrega salto de línea HTML
 
             } else {
-                letra=(letra=='|')?' ':letra;
+                letra = (letra == '|') ? ' ' : letra;
                 target.innerHTML += letra;
             }
 
-            textAux+=[' ','\s', '\n', '\t'].includes(letraSec)? '':letraSec;
+            textAux += [' ', '\s', '\n', '\t'].includes(letraSec) ? '' : letraSec;
 
-            if  (textAux in colorMetria){
-                target.innerHTML ='';
-                if(textAux=='iProgramIt()'){
+            if (textAux in colorMetria) {
+                target.innerHTML = '';
+                if (textAux == 'iProgramIt()') {
                     targetFinal.innerHTML += `<span class="${colorMetria[textAux]}"><br>&nbsp;&nbsp;&nbsp;&nbsp;${textAux}</span>`;
-                }else if(textAux==';'){
+                } else if (textAux == ';') {
                     targetFinal.innerHTML += `<span class="${colorMetria[textAux]}">${textAux}</span><br>`;
-                }else if(textAux.includes('|')){
-                    targetFinal.innerHTML += `<span class="${colorMetria[textAux]}">${textAux.replaceAll('|',' ')}</span>`;
-                }else{
+                } else if (textAux.includes('|')) {
+                    targetFinal.innerHTML += `<span class="${colorMetria[textAux]}">${textAux.replaceAll('|', ' ')}</span>`;
+                } else {
                     targetFinal.innerHTML += `<span class="${colorMetria[textAux]}">${textAux}</span>`;
                 }
 
-                textAux='';
+                textAux = '';
             }
         }, delay);
     }
+    setTimeout(() => {
+        if (animacion_change_top) {
+            animacion_change_top.classList.add('animate-desplazar-left');
+        }
+        if (animacion_change_bottom) {
+            animacion_change_bottom.classList.add('animate-desplazar-rigth');
+        }
+    }, delay + 500);
 });
