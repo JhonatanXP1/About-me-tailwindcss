11e88cd54a3ad4d311d4b5b1922f5143e72cc278
JhonatanEducapp <jdiaz@sistemaeducapp.com>
2025-11-15 13:55:28 -0600

    UPDATE- Animacion de starts

--- pruebas/public/holeBlack.js
+++ pruebas/public/holeBlack.js
@@ -59,4 +59,105 @@ window.addEventListener('load', function () {
         ctx.scale(scaleFactor, scaleFactor);
     }
 
+    function rotate(cx, cy, x, y, angle) {
+        const radians = angle;
+        const cos = Math.cos(radians);
+        const sin = Math.sin(radians);
+        const nx = (cos * (x - cx)) + (sin * (y - cy)) + cx;
+        const ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
+        return [nx, ny];
+    }
+
+    class Star {
+        constructor() {
+            const rands = [];
+            rands.push(Math.random() * (maxorbit / 2) + 1);
+            rands.push(Math.random() * (maxorbit / 2) + maxorbit);
+
+            this.orbital = (rands.reduce((p, c) => p + c, 0) / rands.length);
+            
+            this.x = centerX;
+            this.y = centerY + this.orbital;
+
+            this.yOrigin = centerY + this.orbital;
+
+            this.speed = (Math.floor(Math.random() * 2.5) + 1.5) * Math.PI / 180;
+            this.rotation = 0; // current Rotation
+            this.startRotation = (Math.floor(Math.random() * 360) + 1) * Math.PI / 180;
+
+            this.id = stars.length;
+
+            this.collapseBonus = this.orbital - (maxorbit * 0.7);
+            if (this.collapseBonus < 0) {
+                this.collapseBonus = 0;
+            }
+
+            this.color = 'rgba(255,255,255,' + (1 - ((this.orbital) / 255)) + ')';
+            this.hoverPos = centerY + (maxorbit / 2) + this.collapseBonus;
+            this.expansePos = centerY + (this.id % 100) * -10 + (Math.floor(Math.random() * 20) + 1);
+
+            this.prevR = this.startRotation;
+            this.prevX = this.x;
+            this.prevY = this.y;
+            
+            this.originalY = this.yOrigin;
+
+            stars.push(this);
+        }
+
+        draw() {
+            if (!expanse && !returning) {
+                this.rotation = this.startRotation + (currentTime * this.speed);
+                if (!collapse) {
+                    if (this.y > this.yOrigin) {
+                        this.y -= 2.5;
+                    }
+                    if (this.y < this.yOrigin - 4) {
+                        this.y += (this.yOrigin - this.y) / 10;
+                    }
+                } else { // on hover
+                    this.trail = 1;
+                    if (this.y > this.hoverPos) {
+                        this.y -= (this.hoverPos - this.y) / -5;
+                    }
+                    if (this.y < this.hoverPos - 4) {
+                        this.y += 2.5;
+                    }
+                }
+            } else if (expanse && !returning) {
+                this.rotation = this.startRotation + (currentTime * (this.speed / 2));
+                if (this.y > this.expansePos) {
+                    this.y -= Math.floor(this.expansePos - this.y) / -80;
+                }
+            } else if (returning) {
+                // Returning to original orbit slowly
+                this.rotation = this.startRotation + (currentTime * this.speed);
+                if (Math.abs(this.y - this.originalY) > 2) {
+                    this.y += (this.originalY - this.y) / 50; // Much slower return
+                } else {
+                    this.y = this.originalY;
+                    this.yOrigin = this.originalY;
+                }
+            }
+
+            ctx.save();
+            ctx.fillStyle = this.color;
+            ctx.strokeStyle = this.color;
+            ctx.beginPath();
+            const oldPos = rotate(centerX, centerY, this.prevX, this.prevY, -this.prevR);
+            ctx.moveTo(oldPos[0], oldPos[1]);
+            ctx.translate(centerX, centerY);
+            ctx.rotate(this.rotation);
+            ctx.translate(-centerX, -centerY);
+            ctx.lineTo(this.x, this.y);
+            ctx.stroke();
+            ctx.restore();
+
+            this.prevR = this.rotation;
+            this.prevX = this.x;
+            this.prevY = this.y;
+        }
+    }
+
+
 });
